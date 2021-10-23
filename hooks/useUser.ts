e14be26f27d8029bad3d7f3a6a407308b6d4278b
Mohammad Baqer <mtbaqer@gmail.com>
2021-10-22 23:18:44 -0400

    made users sign in anonymously to get their user id

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,12 +1,36 @@
 import { atom, useAtom } from "jotai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User } from "../types/types";
+import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 
-const userAtom = atom<User | null>(null);
+const auth = getAuth();
+
+const userIdAtom = atom<string | null>(null);
 
 export default function useUser() {
-  const [user, setUser] = useAtom(userAtom);
+  // const [user, setUser] = useAtom(userAtom);
+  const [userId, setUserId] = useAtom(userIdAtom);
 
-  return { user, setUser };
+  useEffect(() => {
+    subscribeToSignIn();
+  }, []);
 
+  function subscribeToSignIn() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("loggedIn");
+
+        setUserId(user.uid);
+      } else {
+        console.log("asked to sign in");
+        signIn();
+      }
+    });
+  }
+
+  function signIn() {
+    signInAnonymously(auth);
+  }
+
+  return { userId };
 }
