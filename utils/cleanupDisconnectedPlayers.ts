d0ecf57a54c6e7860064a80478c2386ed9d420c3
Mohammad Baqer <mtbaqer@gmail.com>
2022-03-21 19:40:23 -0400

    fix the bug where connectedPlayersTimestamps could be undefined.

--- utils/cleanupDisconnectedPlayers.ts
+++ utils/cleanupDisconnectedPlayers.ts
@@ -3,7 +3,7 @@ import { Team, User } from "../types/types";
 export default function cleanupDisconnectedPlayers(team: Team, connectedPlayersTimestamps: Record<string, User>) {
   team.members = team.members ?? [];
   team.members = team.members.filter((timestamp, index) => {
-    const isConnected = timestamp in connectedPlayersTimestamps;
+    const isConnected = connectedPlayersTimestamps && timestamp in connectedPlayersTimestamps;
     if (!isConnected && index <= team.currentMemberIndex) team.currentMemberIndex--;
     return isConnected;
   });
