70c6bf57f51fd148db1ff6b68378f8effaaa8001
Mohammad Baqer <mtbaqer@gmail.com>
2022-03-23 06:05:57 -0400

    add Confetti to correct feedback animation

--- components/Board/PlayArea/Feedback.tsx
+++ components/Board/PlayArea/Feedback.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import Image from "next/image";
 import useDidMountEffect from "../../../hooks/useDidMount";
 import { motion, useAnimation } from "framer-motion";
 import styled from "styled-components";
 import { useAtomValue } from "jotai/utils";
 import { roomAtom } from "../../../atoms/room";
+import Confetti from "./Confetti";
 
 interface Props {}
 
@@ -12,11 +13,14 @@ const Feedback: FunctionComponent<Props> = ({}) => {
   const controls = useAnimation();
   const room = useAtomValue(roomAtom);
 
+  const [animateConfetti, setAnimateConfetti] = useState(false);
+
   useDidMountEffect(() => {
     animate();
   }, [room.teams[room.currentTeamIndex].score]);
 
   async function animate() {
+    setAnimateConfetti(true);
     await controls.start({
       opacity: 1,
       transition: { duration: 0 },
@@ -38,12 +42,16 @@ const Feedback: FunctionComponent<Props> = ({}) => {
       scale: 1,
       transition: { duration: 0.05 },
     });
+    setAnimateConfetti(false);
   }
 
   return (
     <FeedbackContainer animate={controls}>
       {room.lastGuess ? (
-        <Image src="/images/correct.svg" alt="correct" width={96} height={111} />
+        <SubContainer>
+          <Image src="/images/correct.svg" alt="correct" width={96} height={111} />
+          <Confetti animate={animateConfetti} />
+        </SubContainer>
       ) : (
         <Image src="/images/wrong.svg" alt="wrong" width={96} height={111} />
       )}
@@ -58,6 +66,11 @@ const FeedbackContainer = styled(motion.div)`
   top: 40%;
   left: 48%;
   opacity: 0;
+  pointer-events: none;
+`;
+
+const SubContainer = styled.div`
+  position: relative;
 `;
 
 export default Feedback;
