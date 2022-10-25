28a188c3d9337f378a0c82199a42ed6f795d12a2
Mohammad Baqer <mtbaqer@gmail.com>
2022-10-25 01:04:45 -0400

    finished setup for testing

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/index";
+import * as useNewRoomActionsModule from "../hooks/useNewRoomActions";
+import Home from "../pages";
+import userEvent from "@testing-library/user-event";
 
 describe("Home", () => {
-  it("renders a heading", () => {
+  it("should render the generate room button", () => {
+    jest.spyOn(useNewRoomActionsModule, "default").mockReturnValue({ navigateToNewRoom: jest.fn() });
+
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
+    const button = screen.getByRole("button", { name: "play button GENERATE ROOM" });
 
-    expect(heading).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should navigate to new room when generate room button is pressed", async () => {
+    const mockNavigateToNewRoom = jest.fn();
+    jest.spyOn(useNewRoomActionsModule, "default").mockReturnValue({ navigateToNewRoom: mockNavigateToNewRoom });
+
+    const user = userEvent.setup();
+
+    render(<Home />);
+
+    const button = screen.getByRole("button", { name: "play button GENERATE ROOM" });
+    await user.click(button);
+
+    expect(mockNavigateToNewRoom).toHaveBeenCalledTimes(1);
   });
 });
