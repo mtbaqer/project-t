e69a8357f5083538eab49381fd440b858383c599
Brad Donohue <bdonohue96@gmail.com>
2022-11-12 13:18:01 -0500

    Took out winning team from results table

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -1,49 +1,51 @@
-import {FunctionComponent} from "react";
+import { FunctionComponent } from "react";
 import BackButton from "./BackButton";
-import {roomAtom} from "../../atoms/room";
-import {useAtomValue} from "jotai/utils";
+import { roomAtom } from "../../atoms/room";
+import { useAtomValue } from "jotai/utils";
 import styled from "styled-components";
 
-interface Props {
-}
+interface Props {}
 
 const Results: FunctionComponent<Props> = ({}) => {
-    const room = useAtomValue(roomAtom);
-    const teams = room.teams;
-    const spectators = teams[0];
-    let activeTeams: { teamNumber: number; score: number }[] = [];
-    teams.map((item, index) => {
-        if (index > 0) {
-            const team = {teamNumber: index, score: item.score};
-            activeTeams.push(team);
-        }
-    });
+  const room = useAtomValue(roomAtom);
+  const teams = room.teams;
+  const spectators = teams[0];
+  let activeTeams: { teamNumber: number; score: number }[] = [];
+  teams.map((item, index) => {
+    if (index > 0) {
+      const team = { teamNumber: index, score: item.score };
+      activeTeams.push(team);
+    }
+  });
 
-    activeTeams.sort((a, b) => b.score - a.score);
-    return (
-        <>
-            <BackButton/>
-            <Title>Congratulations!</Title>
-            <Winner>
-                {" "}
-                Team {activeTeams[0].teamNumber} is the winner with {activeTeams[0].score} points!
-            </Winner>
-            <TeamsTable>
-                <tbody>
-                {activeTeams.map((team) => {
-
-                    return (
-                        <tr>
-                            <TableDataCell>{`Team ${team.teamNumber} `}</TableDataCell>
-                            <TableDataCell>{`${team.score} points`} </TableDataCell>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </TeamsTable>
-            {/* <TestArea /> */}
-        </>
-    );
+  activeTeams.sort((a, b) => b.score - a.score);
+  return (
+    <>
+      <BackButton />
+      <Title>Congratulations!</Title>
+      <Winner>
+        {" "}
+        Team {activeTeams[0].teamNumber} is the winner with {activeTeams[0].score} points!
+      </Winner>
+      <TeamsTable>
+        <tbody>
+          {activeTeams.map((team, index) => {
+            if (index > 0) {
+              return (
+                <tr>
+                  <TableDataCell>{`Team ${team.teamNumber} `}</TableDataCell>
+                  <TableDataCell>{`${team.score} points`} </TableDataCell>
+                </tr>
+              );
+            } else {
+              return;
+            }
+          })}
+        </tbody>
+      </TeamsTable>
+      {/* <TestArea /> */}
+    </>
+  );
 };
 
 const Title = styled.div`
@@ -69,7 +71,6 @@ const TeamsTable = styled.table`
   font-family: "Nunito";
   font-weight: bold;
   font-size: 20px;
-
 `;
 const TableDataCell = styled.td`
   font-family: "Nunito";
@@ -77,7 +78,6 @@ const TableDataCell = styled.td`
   font-size: 20px;
   border: 5px solid cornflowerblue;
   text-align: center;
-
 `;
 
 export default Results;
