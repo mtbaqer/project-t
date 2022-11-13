68743f2b9ad520bdf3dd54463dc0618cf2b9c400
Stephen Lin <yl3248@drexel.edu>
2022-11-12 21:39:07 -0500

    Result Page: fix some layout and code duplication

---
+++ components/Results/ResultSettingArea.tsx
@@ -0,0 +1,84 @@
+import {FunctionComponent} from "react";
+import BackButton from "../../components/Board/HUD/BackButton";
+import {roomAtom} from "../../atoms/room";
+import {useAtomValue} from "jotai/utils";
+import styled from "styled-components";
+
+interface Props {
+}
+
+const ResultSettingArea: FunctionComponent<Props> = ({}) => {
+    const room = useAtomValue(roomAtom);
+    const teams = room.teams;
+    const spectators = teams[0];
+    let activeTeams: { teamNumber: number; score: number }[] = [];
+    teams.map((item, index) => {
+        if (index > 0) {
+            const team = {teamNumber: index, score: item.score};
+            activeTeams.push(team);
+        }
+    });
+
+    activeTeams.sort((a, b) => b.score - a.score);
+    return (
+        <>
+            <BackButton/>
+            <Title>Congratulations!</Title>
+            <Winner>
+                Team {activeTeams[0].teamNumber} is the winner with {activeTeams[0].score} points!
+            </Winner>
+            <TeamsTable>
+                <tbody>
+                {activeTeams.map((team, index) => {
+                    if (index > 0) {
+                        return (
+                            <tr>
+                                <TableDataCell>{`Team ${team.teamNumber} `}</TableDataCell>
+                                <TableDataCell>{`${team.score} points`} </TableDataCell>
+                            </tr>
+                        );
+                    } else {
+                        return;
+                    }
+                })}
+                </tbody>
+            </TeamsTable>
+        </>
+    );
+};
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Nunito";
+  font-weight: bold;
+  font-size: 100px;
+`;
+
+const Winner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Nunito";
+  font-weight: bold;
+  font-size: 20px;
+`;
+const TeamsTable = styled.table`
+  font-family: "Nunito";
+  font-weight: bold;
+  font-size: 20px;
+
+`;
+const TableDataCell = styled.td`
+  font-family: "Nunito";
+  font-weight: bold;
+  font-size: 20px;
+  border: 5px solid cornflowerblue;
+  text-align: center;
+
+`;
+
+export default ResultSettingArea;
--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -1,83 +1,18 @@
 import {FunctionComponent} from "react";
-import BackButton from "./BackButton";
-import {roomAtom} from "../../atoms/room";
-import {useAtomValue} from "jotai/utils";
-import styled from "styled-components";
+import ResultSettingArea from "./ResultSettingArea";
 
 interface Props {
 }
 
 const Results: FunctionComponent<Props> = ({}) => {
-    const room = useAtomValue(roomAtom);
-    const teams = room.teams;
-    const spectators = teams[0];
-    let activeTeams: { teamNumber: number; score: number }[] = [];
-    teams.map((item, index) => {
-        if (index > 0) {
-            const team = {teamNumber: index, score: item.score};
-            activeTeams.push(team);
-        }
-    });
-
-    activeTeams.sort((a, b) => b.score - a.score);
     return (
         <>
-            <BackButton/>
-            <Title>Congratulations!</Title>
-            <Winner>
-                {" "}
-                Team {activeTeams[0].teamNumber} is the winner with {activeTeams[0].score} points!
-            </Winner>
-            <TeamsTable>
-                <tbody>
-                {activeTeams.map((team) => {
-
-                    return (
-                        <tr>
-                            <TableDataCell>{`Team ${team.teamNumber} `}</TableDataCell>
-                            <TableDataCell>{`${team.score} points`} </TableDataCell>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </TeamsTable>
-            {/* <TestArea /> */}
+            <ResultSettingArea/>
         </>
     );
 };
 
-const Title = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: "Nunito";
-  font-weight: bold;
-  font-size: 100px;
-`;
-
-const Winner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: "Nunito";
-  font-weight: bold;
-  font-size: 20px;
-`;
-const TeamsTable = styled.table`
-  font-family: "Nunito";
-  font-weight: bold;
-  font-size: 20px;
-
-`;
-const TableDataCell = styled.td`
-  font-family: "Nunito";
-  font-weight: bold;
-  font-size: 20px;
-  border: 5px solid cornflowerblue;
-  text-align: center;
-
-`;
-
 export default Results;
+
+
+
