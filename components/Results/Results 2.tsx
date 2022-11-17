d5931a80725be4d671827dfc668f464a3fcda1ba
Brad Donohue <bdonohue96@gmail.com>
2022-11-16 19:11:38 -0500

    Removed ResultsSettingArea component

---
+++ components/Results/Results 2.tsx
@@ -0,0 +1,18 @@
+import {FunctionComponent} from "react";
+import ResultSettingArea from "./ResultSettingArea";
+
+interface Props {
+}
+
+const Results: FunctionComponent<Props> = ({}) => {
+    return (
+        <>
+            <ResultSettingArea/>
+        </>
+    );
+};
+
+export default Results;
+
+
+
