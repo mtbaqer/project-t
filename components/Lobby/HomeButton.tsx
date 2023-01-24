a93321d4ee2eae8302e9b8947e13bbfb3e42a621
Stephen Lin <81601143+Zero-stars@users.noreply.github.com>
2023-01-23 22:13:50 -0500

    Add a new file for a HomeButton that will take user back to the Generate New Room page.

---
+++ components/Lobby/HomeButton.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import useRoomActions from "hooks/useRoomActions";
+import Button from "../Button";
+
+interface Props {
+}
+
+const HomeButton: FunctionComponent<Props> = ({}) => {
+  const { onReturnToGenerateRoom } = useRoomActions();
+  return (
+    <Container>
+      <Button text="RETURN TO GENERATE ROOM" onClick={onReturnToGenerateRoom} imageSource="/images/chevron-left.svg" />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+export default HomeButton;
