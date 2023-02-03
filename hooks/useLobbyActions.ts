ad6ba66b72ac0bf9df42f1568d2ff7733f171779
jg3835 <97192074+jg3835@users.noreply.github.com>
2023-02-03 09:31:38 -0500

    Set restrictions; must have more than one team, more than 1 person per team and only 12 players in game.

--- hooks/useLobbyActions.ts
+++ hooks/useLobbyActions.ts
@@ -1,7 +1,5 @@
-import { child, getDatabase, increment, ref, runTransaction, update } from "firebase/database";
-import { useAtomValue } from "jotai/utils";
+import { child, getDatabase, ref, runTransaction, update } from "firebase/database";
 import { useRouter } from "next/router";
-import { roomAtom } from "../atoms/room";
 import { DefaultTeam } from "../constants/room";
 import { Room, RoomStatus, Team } from "../types/types";
 import cleanupDisconnectedPlayers from "../utils/cleanupDisconnectedPlayers";
@@ -11,6 +9,8 @@ const database = getDatabase();
 export default function useLobbyActions() {
   const router = useRouter();
   const { roomId } = router.query;
+  let canBeStarted = true;
+  let numberOfTotalTeamPlayers: number;
 
   const roomRef = ref(database, `rooms/${roomId}`);
   const teamsRef = child(roomRef, "teams");
@@ -44,8 +44,19 @@ export default function useLobbyActions() {
   }
 
   function onStartGame() {
-    const status: RoomStatus = "waiting";
-    update(roomRef, { status });
+    runTransaction(teamsRef, (teams: Team[]) => {
+      if (teams.length < 2) canBeStarted = false;
+      teams.forEach((team, index) => {
+        if (index != 0 && team.members?.length < 2) canBeStarted = false;
+        if (index != 0) numberOfTotalTeamPlayers += team.members.length;
+        if (numberOfTotalTeamPlayers > 12) canBeStarted = false;
+      });
+      return teams;
+    });
+    if (canBeStarted) {
+      const status: RoomStatus = "waiting";
+      update(roomRef, { status });
+    }
   }
 
   async function onCopyLink() {
