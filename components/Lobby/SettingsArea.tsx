0c0fcc0e17af5aeb39932df4803982cba5a6ea22
Mohammad Baqer <mtbaqer@gmail.com>
2023-02-16 13:45:38 -0500

    styled settings area

--- components/Lobby/SettingsArea.tsx
+++ components/Lobby/SettingsArea.tsx
@@ -6,6 +6,8 @@ import useLobbyActions from "../../hooks/useLobbyActions";
 import { ScreenSizes } from "../../Theme/ScreenSizes";
 import { Spaces } from "../../Theme/Spaces";
 import Button from "../Button";
+import Panel from "../Panel";
+import Text from "../Text";
 
 interface Props {}
 
@@ -27,36 +29,38 @@ const SettingsArea: FunctionComponent<Props> = ({}) => {
   return (
     <Container>
       <SubContainer>
-        <ButtonContainer>
-          <label>
-            Number of teams:
-            <select value={String(room.teams.length - 1)} onChange={(e) => onSetNumberOfTeams(e.target.value)}>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-            </select>
-          </label>
-          <label>
-            Number of rounds:
-            <select value={String(room.settings.maxRounds)} onChange={(e) => onSetNumberOfRounds(e.target.value)}>
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-              <option value="20">20</option>
-            </select>
-          </label>
-          <label>
-            Time per round:
-            <select value={String(room.settings.timePerRound)} onChange={(e) => onSetTimePerRound(e.target.value)}>
-              <option value="30">30 s</option>
-              <option value="45">45 s</option>
-              <option value="60">60 s</option>
-              <option value="75">75 s</option>
-            </select>
-          </label>
-        </ButtonContainer>
+        <Panel title="Settings" titleSize="l">
+          <SettingsSubContainer>
+            <SelectContainer>
+              <Text size="s">Number of teams:</Text>
+              <Select value={String(room.teams.length - 1)} onChange={(e) => onSetNumberOfTeams(e.target.value)}>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </Select>
+            </SelectContainer>
+            <SelectContainer>
+              <Text size="s">Number of rounds:</Text>
+              <Select value={String(room.settings.maxRounds)} onChange={(e) => onSetNumberOfRounds(e.target.value)}>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+              </Select>
+            </SelectContainer>
+            <SelectContainer>
+              <Text size="s">Time per round:</Text>
+              <Select value={String(room.settings.timePerRound)} onChange={(e) => onSetTimePerRound(e.target.value)}>
+                <option value="30">30 s</option>
+                <option value="45">45 s</option>
+                <option value="60">60 s</option>
+                <option value="75">75 s</option>
+              </Select>
+            </SelectContainer>
+          </SettingsSubContainer>
+        </Panel>
 
         <ButtonContainer>
           <Button onClick={handleCopyLink} text={isCopied ? "LINK COPIED!" : "COPY LINK"} />
@@ -87,6 +91,30 @@ const SubContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: ${Spaces.medium};
+`;
+
+const SettingsSubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${Spaces.small};
+  align-items: center;
+`;
+
+const SelectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 ${Spaces.small};
+`;
+
+const Select = styled.select`
+  border: 2px solid black;
+  box-shadow: black 0 1px 0 0;
+  border-radius: 5px;
+
+  &:focus {
+    outline: 2px solid #eec408; //TODO: use theme color
+  }
 `;
 
 const ButtonContainer = styled.div`
